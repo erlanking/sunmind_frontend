34d30c9a558d277e0440cf6ebfba8de37ac9709e
erlanking <bekboliev333@gmail.com>
2026-05-23 16:25:57 +0600

    fix: add User type to types/index

--- sunmind-frontend/types/index.ts
+++ sunmind-frontend/types/index.ts
@@ -1,5 +1,12 @@
 export type TimePeriod = 'day' | 'week' | 'month';
 
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  roles?: { role_name: string }[];
+}
+
 export interface Review {
   id: string;
   userId: string;
